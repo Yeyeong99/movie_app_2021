abef7e7680f2a4bf44bc7bc9fcf3c9d8f0408aeb
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-01-30 01:04:51 +0900

    react function practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const questionList = [
+  {
+    explanation : "hello",
+    detail : "what"
+  },
+  {
+    explanation : "second",
+    detail : "second"
+  }
+]
+
+function Quest({explanation, detail}) {
+  return (
+    <div>
+      <h1>{explanation}</h1>
+      <h2>{detail}</h2>
+    </div>
+  )
+}
 
 function App() {
-  return <div><h1>Hello</h1>Hello</div>;
+
+  return (
+    <div>
+      {questionList.map(quest =>
+        <Quest explanation = {quest.explanation} detail = {quest.detail}/>
+  
+      )}
+    </div>
+  )
 }
 
 export default App;
